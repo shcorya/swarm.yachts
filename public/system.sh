29cc0c7c51662ec6528d1c4157e18dc61ee72b3e
Stehpen Corya <stephen@corya.net>
2025-12-30 16:57:22 -0500

    update system stack to new copy-paste paradigm

---
+++ public/system.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+cat << EOF | docker stack deploy -c - system
+services:
+  prune:
+    image: docker
+    command: ["docker", "system", "prune", "-f"]
+    volumes:
+      - "/var/run/docker.sock:/var/run/docker.sock"
+    deploy:
+      mode: global
+      labels:
+        - "swarm.cronjob.enable=true"
+        - "swarm.cronjob.schedule=0 0 0 * * *"
+        - "swarm.cronjob.skip-running=false"
+      restart_policy:
+        condition: none
+
+  updates:
+    image: ixdotai/cioban
+    volumes:
+      - '/var/run/docker.sock:/var/run/docker.sock'
+    deploy:
+      placement:
+        constraints:
+          - node.role == manager
+
+EOF
