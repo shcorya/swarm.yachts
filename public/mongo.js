84031c066435ed5fdcb47e74d43ad72996654318
Stephen Corya <stephen@corya.net>
2026-07-31 13:26:20 -0400

    input checking

--- public/mongo.js
+++ public/mongo.js
@@ -1,4 +1,5 @@
 import { Socket } from "jsr:@typescriptplayground/socket";
+import { promptSecret } from "jsr:@deno-cli-tools/prompts";
 
 // variables for fetch methods and continuation
 let response, result, proceed;
@@ -7,17 +8,22 @@ let response, result, proceed;
 const dockSock = new Socket('/var/run/docker.sock');
 
 // prompt for TLD (used for certbot for mongo express)
-const tld = prompt("Enter your swarm's TLD (e.g. \"swarm.yachts\"): ").replaceAll('\"', '').replaceAll('\'', '');
+let tld = prompt("Enter your swarm's TLD (e.g. \"swarm.yachts\"): ");
 if (!tld) {
     console.error("Top-level domain is required.");
     Deno.exit(1);
+} else {
+    tld = tld.replaceAll('\"', '').replaceAll('\'', '');
 }
 
 // get the list of hostnames of nodes to label, cleaning up quotes and splitting into an array
-const labelUs = prompt("Enter a space-seperated array of node hostnames that will hold data: ")
-.replaceAll('\"', '')
-.replaceAll('\'', '')
-.split(' ');
+let labelUs = prompt("Enter a space-seperated array of node hostnames that will hold data: ");
+if (!labelUs) {
+    console.error("An array of node hostnames is required.");
+    Deno.exit(1);
+} else {
+    labelUs = labelUs.replaceAll('\"', '').replaceAll('\'', '').split(' ');
+}
 // some sanity checking
 if (labelUs.length !== 3 || labelUs.length !== 5 || labelUs.length !== 7) {
     console.error("It is highly recommended to use 3, 5 or 7 nodes for high availability.");
@@ -30,6 +36,38 @@ if (labelUs.length !== 3 || labelUs.length !== 5 || labelUs.length !== 7) {
 // prompt for label that will be used to identify nodes with local volumes (has default value "yachts.swarm.mongo=db")
 const label =  prompt("Enter mongo label key and value (defaults to \"yachts.swarm.mongo=db\"): ") || "yachts.swarm.mongo=db";
 const [labelKey, labelValue] = label.replaceAll('\"', '').replaceAll('\'', '').split('=');
+
+// get Mongo Express password
+const expressUser = prompt("Enter a username for Mongo Express basic auth (defaults to \"mongo\"): ")
+.replaceAll('\"', '').replaceAll('\'', '') || "mongo";
+const expressPassword = await promptSecret("Enter a password for Mongo Express: ");
+const expressPassword2 = await promptSecret("Re-enter password: ");
+if (!expressPassword) {
+    console.error("A Mongo Express password is required.");
+    Deno.exit(4);
+}
+if (expressPassword !== expressPassword2) {
+    console.error("Passwords do not match.");
+    Deno.exit(3);
+}
+const expressSecretName = prompt("Enter a name for the Mongo Express password secret (defaults to \"mongo_express_pw\"):")
+.replaceAll('\"', '').replaceAll('\'', '') || "mongo_express_pw";
+
+// get status page secret
+const statusUser = prompt("Enter a username for the HAProxy status page (defaults to \"mongo\"): ")
+.replaceAll('\"', '').replaceAll('\'', '') || "mongo";
+const statusPassword = await promptSecret("Enter a password for the HAProxy status page: ");
+const statusPassword2 = await promptSecret("Re-enter password: ");
+if (!statusPassword) {
+    console.error("An HAProxy status page password is required.");
+    Deno.exit(4);
+}
+if (statusPassword !== statusPassword2) {
+    console.error("Passwords do not match.");
+    Deno.exit(3);
+}
+const statusSecretName = prompt("Enter a name for the HAProxy status password secret (defaults to \"mongo_status_pw\"): ")
+.replaceAll('\"', '').replaceAll('\'', '') || "mongo_status_pw";
 
 // get list of all nodes
 response = await dockSock.request('/nodes', {
@@ -46,7 +84,7 @@ for (let host of labelUs) {
     const update = {
         Labels: {
             ...updateMe.Spec.Labels,
-            labelKey: labelValue
+            [labelKey]: labelValue
         }
     }
     response = await dockSock.request(`/nodes/${updateMe.ID}/update?version=${updateMe.Version.Index}`, {
@@ -110,7 +148,7 @@ listen stats
         bind *:7000 ssl crt {{ env "HAPROXY_CERT_FILE" }}
         stats enable
         stats uri /
-        stats auth mongo:{{ secret "mongo_status_pw" }}
+        stats auth ${statusUser}:{{ secret "${statusSecretName}" }}
         stats refresh 30s
 
 frontend mongo_primary
@@ -140,7 +178,7 @@ backend mongo_stack
         tcp-check send-binary 00 # Term
         tcp-check expect binary 69736d61737465720001 #ismaster True
         default-server inter 3s fall 3 rise 2 on-marked-down shutdown-sessions
-${labeledNodes.map(node => `        server ${node.Description.Hostname} ${node.ID}.mongodb.internal:27017 check inter 3s fall 3 rise 2`)}
+${labeledNodes.map(node => `        server ${node.Description.Hostname} ${node.ID}.mongodb.internal:27017 check inter 3s fall 3 rise 2`).join('\n')}
 `;
 
 const composeTemplate = `
@@ -184,13 +222,13 @@ services:
     ports:
       - "8081:8081"
     secrets:
-      - mongo_express_pw
+      - ${expressSecretName}
     volumes:
       - certs:/mnt/certs/
     environment:
       ME_CONFIG_MONGODB_URL: mongodb://mongodb.internal:27017
       ME_CONFIG_BASICAUTH_USERNAME: mongo
-      ME_CONFIG_BASICAUTH_PASSWORD_FILE: /run/secrets/mongo_express_pw
+      ME_CONFIG_BASICAUTH_PASSWORD_FILE: /run/secrets/${expressSecretName}
       ME_CONFIG_SITE_SSL_ENABLED: "true"
       ME_CONFIG_SITE_SSL_CRT_PATH: /mnt/certs/live/${tld}/cert.pem
       ME_CONFIG_SITE_SSL_KEY_PATH: /mnt/certs/live/${tld}/privkey.pem
@@ -259,7 +297,9 @@ configs:
     external: true
 
 secrets:
-  mongo_express_pw:
+  ${expressSecretName}:
+    external: true
+  ${statusSecretName}:
     external: true
 
 volumes:
